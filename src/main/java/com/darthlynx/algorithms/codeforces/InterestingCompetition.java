b3e981612ae76a59f49131fede373cfad6af528d
Ihor Khlaponin <igor.boxmails@gmail.com>
2021-03-08 01:35:19 +0200

    codeforces: Interesting Competition: accepted

--- src/main/java/com/darthlynx/algorithms/codeforces/InterestingCompetition.java
+++ src/main/java/com/darthlynx/algorithms/codeforces/InterestingCompetition.java
@@ -10,7 +10,6 @@ import java.util.List;
 public class InterestingCompetition {
 
     public static char OPEN = '(';
-    public static char CLOSE = ')';
 
     public static void main(String[] args) {
         List<String> inputs = getInputData();
@@ -24,36 +23,35 @@ public class InterestingCompetition {
         if (n % 2 != 0) {
             return -1;
         }
+
         int depth = 0;
-        for (char ch : sequence) {
-            if (ch == OPEN) {
+        int start = -1;
+        int movements = 0;
+        int pointer = 0;
+        while (pointer < sequence.length) {
+            if (sequence[pointer] == OPEN) {
                 depth++;
             } else {
                 depth--;
             }
+
+            if (depth < 0) {
+                if (start < 0) {
+                    start = pointer; // update start position for unbalanced subsequence
+                }
+            } else if (depth == 0 && start >= 0) {  // if sequence becomes balanced and we have the start of unbalanced sequence,
+                movements += (pointer - start + 1); // count the number of movements required
+                start = -1;                         // and update the start (we don't have unbalanced subsequence at this time)
+            }
+            pointer++;
         }
-        if (depth != 0) {
+
+        if (depth != 0) { // sequence is unbalanced
             return -1;
-        }
-
-        char[] balanced = new char[sequence.length];
-        int movements = 0;
-        for (int i = 0; i < sequence.length; i++){
-
         }
 
         return movements;
     }
-
-    // input
-    // 8
-    // ))((())(
-    // output 6
-
-    // input
-    // 3
-    // (()
-    // output -1
 
     private static List<String> getInputData() {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
