b75abd9b68bafb492307e8cdc06370bb79257dc5
Ihor Khlaponin <igor.boxmails@gmail.com>
2021-04-14 12:26:30 +0300

    refactoring + divisors calculation and testing

--- src/main/java/com/darthlynx/algorithms/timus/AmbitiousExperiment.java
+++ src/main/java/com/darthlynx/algorithms/timus/AmbitiousExperiment.java
@@ -3,6 +3,8 @@ package com.darthlynx.algorithms.timus;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 // https://timus.online/problem.aspx?space=1&num=2062&locale=ru
@@ -12,6 +14,7 @@ public class AmbitiousExperiment {
     int[] particlesChargeDeltas;
     int[] sqrtBlocks;
     int sqrtSize;
+    List<Integer>[] particlesDivisors;
 
     public static void main(String[] args) {
         boolean onlineJudge = System.getProperty("ONLINE_JUDGE") != null;
@@ -24,25 +27,27 @@ public class AmbitiousExperiment {
     }
 
     public void solve(Scanner in) {
-        int n = in.nextInt(); // number of particles
+        int n = in.nextInt(); // number of particles, 1 <= n <= 3*10^5
 
-        int[] particles = new int[n+1];
+        particlesDivisors = calculateParticlesDivisors(n);
+
+        int[] particlesCharges = new int[n+1];
         for (int i = 0; i < n; i++) {
-            particles[i] = in.nextInt();
+            particlesCharges[i] = in.nextInt();
         }
 
         particlesChargeDeltas = new int[n+1];
         sqrtSize = (int)(Math.sqrt(n) + 1);
         sqrtBlocks = new int[sqrtSize+1];
 
-        int q = in.nextInt(); // number of actions over particles
+        int q = in.nextInt(); // number of actions over particles, 1 <= q <= 3*10^5
 
         for (int i = 0; i < q; i++) {
             int start = in.nextInt();
             int index;
             if (start == 1) {
-                index = in.nextInt();
-                System.out.println(particlesChargeDeltas[index] + sqrtBlocks[index/sqrtSize] + particles[index]);
+                index = in.nextInt() - 1; // because 1 <= n <= 3*10^5
+                System.out.println(particlesChargeDeltas[index] + sqrtBlocks[index/sqrtSize] + particlesCharges[index]);
             } else if (start == 2) {
                 int l = in.nextInt(); // left
                 int r = in.nextInt(); // right
@@ -53,33 +58,46 @@ public class AmbitiousExperiment {
 
     }
 
-    private void updateParticlesCharges(int l, int r, int d) {
-        int leftBlockPos = l/sqrtSize;
-        int rightBlockPos = r/sqrtSize;
+    List<Integer>[] calculateParticlesDivisors(int n) {
+        List<Integer>[] divisors = new List[n+1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = i; j <= n; j+=i) {
+                if (divisors[j] == null) {
+                    divisors[j] = new ArrayList<>();
+                }
+                divisors[j].add(i);
+            }
+        }
+        return divisors;
+    }
+
+    private void updateParticlesCharges(int leftBorder, int rightBorder, int delta) {
+        int leftBlockPos = leftBorder/sqrtSize;
+        int rightBlockPos = rightBorder/sqrtSize;
 
         // if left and right boarder are in the same sqrt block
         if (leftBlockPos == rightBlockPos) {
-            for (int i = l; i <= r; i++) {
-                particlesChargeDeltas[i] += d;
+            for (int i = leftBorder; i <= rightBorder; i++) {
+                particlesChargeDeltas[i] += delta;
             }
         } else {
-            updateLeftBlock(l, leftBlockPos, d);
-            updateBlocksBetweenLeftAndRight(leftBlockPos, rightBlockPos, d);
-            updateRightBlock(r, rightBlockPos, d);
+            updateLeftBlock(leftBorder, leftBlockPos, delta);
+            updateBlocksBetweenLeftAndRight(leftBlockPos, rightBlockPos, delta);
+            updateRightBlock(rightBorder, rightBlockPos, delta);
         }
 
     }
 
-    private void updateLeftBlock(int l, int leftBlockPos, int d) {
+    private void updateLeftBlock(int leftBorder, int leftBlockPos, int d) {
         int rightBorderForLeftBlock = (leftBlockPos+1) * sqrtSize;
-        for (int i = l; i < rightBorderForLeftBlock; i++) {
+        for (int i = leftBorder; i < rightBorderForLeftBlock; i++) {
             particlesChargeDeltas[i] += d;
         }
     }
 
-    private void updateRightBlock(int r, int rightBlockPos, int d) {
+    private void updateRightBlock(int rightBorder, int rightBlockPos, int d) {
         int leftBorderForRightBlock = rightBlockPos * sqrtSize;
-        for (int i = leftBorderForRightBlock; i <= r; i++) {
+        for (int i = leftBorderForRightBlock; i <= rightBorder; i++) {
             particlesChargeDeltas[i] += d;
         }
     }
