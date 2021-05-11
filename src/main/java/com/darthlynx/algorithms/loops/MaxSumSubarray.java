2567307f1bc03ad2716b0dc618a2430a19663762
Ihor Khlaponin <igor.boxmails@gmail.com>
2021-05-12 00:19:15 +0300

    algoacademy: max sum subarray: accepted

---
+++ src/main/java/com/darthlynx/algorithms/loops/MaxSumSubarray.java
@@ -0,0 +1,17 @@
+package com.darthlynx.algorithms.loops;
+
+public class MaxSumSubarray {
+
+    public int maxSumSubarray(int[] nums) {
+
+        int maxSum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                maxSum = Math.max(sum, maxSum);
+            }
+        }
+        return maxSum;
+    }
+}
