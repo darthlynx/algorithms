614ef85e6388334e377986e3de976b8a2b6ce3ee
Ihor Khlaponin <igor.boxmails@gmail.com>
2021-08-03 11:52:39 +0300

    merge two lists

---
+++ src/main/java/com/darthlynx/algorithms/loops/MergeTwoLists.java
@@ -0,0 +1,38 @@
+package com.darthlynx.algorithms.loops;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MergeTwoLists {
+
+    public List<Integer> merge(List<Integer> l1, List<Integer> l2) {
+        List<Integer> result = new ArrayList<>(l1.size() + l2.size());
+
+        int p1 = 0, p2 = 0;
+
+        // O(n+m)
+        int e1, e2;
+        while (p1 < l1.size() || p2 < l2.size()) {
+            if (p1 < l1.size()) {
+                e1 = l1.get(p1);
+            } else {
+                e1 = Integer.MAX_VALUE;
+            }
+            if (p2 < l2.size()) {
+                e2 = l2.get(p2);
+            } else {
+                e2 = Integer.MAX_VALUE;
+            }
+
+            if (e1 <= e2) {
+                result.add(e1);
+                p1++;
+            } else {
+                result.add(e2);
+                p2++;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/java/com/darthlynx/algorithms/loops/MergeTwoListsTest.java
@@ -0,0 +1,25 @@
+package com.darthlynx.algorithms.loops;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class MergeTwoListsTest {
+
+    private MergeTwoLists test = new MergeTwoLists();
+
+    @Test
+    public void test1() {
+        List<Integer> l1 = List.of(1,2,2,5,6,10);
+        List<Integer> l2 = List.of(3,4,7,8,11,12,13);
+
+        List<Integer> expectedResult = List.of(1,2,2,3,4,5,6,7,8,10,11,12,13);
+
+        List<Integer> actualResult = test.merge(l1, l2);
+
+        assertEquals(expectedResult, actualResult);
+    }
+
+}
