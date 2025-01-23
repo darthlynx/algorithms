dd3e7bc787e680e02636cab6df95eb9a3b4541bf
Ihor Khlaponin <igor.boxmails@gmail.com>
2025-01-23 23:50:03 +0000

    neetcode: group anagram

---
+++ src/main/java/com/darthlynx/algorithms/neetcode/arraysAndHashing/AnagramGroups.java
@@ -0,0 +1,43 @@
+package com.darthlynx.algorithms.neetcode.arraysAndHashing;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+// https://neetcode.io/problems/anagram-groups
+public class AnagramGroups {
+
+    class Solution {
+        public List<List<String>> groupAnagrams(String[] strs) {
+            List<List<String>> result = new ArrayList<>();
+            Map<String, List<String>> map = new HashMap<>();
+
+            for (String s : strs) {
+                String key = toKey(s);
+                map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
+            }
+
+            for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+                result.add(entry.getValue());
+            }
+
+            return result;
+        }
+
+        private int[] getChars(String s) {
+            int[] chars = new int[26];
+            for (char ch : s.toCharArray()) {
+                chars[ch - 'a']++;
+            }
+            return chars;
+        }
+
+        private String toKey(String s) {
+            int[] chars = getChars(s);
+            return Arrays.toString(chars);
+        }
+    }
+
+}
