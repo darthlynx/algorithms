4d292320c8777f5ac411039dbc736d9f08c2560d
Ihor Khlaponin <igor.boxmails@gmail.com>
2025-02-17 00:12:09 +0000

    leetcode 875: Koko eating bananas

---
+++ src/main/java/com/darthlynx/algorithms/leetcode/KokoEatingBananas.java
@@ -0,0 +1,38 @@
+package com.darthlynx.algorithms.leetcode;
+
+// https://leetcode.com/problems/koko-eating-bananas/
+public class KokoEatingBananas {
+
+    // Time complexity: O(N*logk)
+    // Space complexity: O(1)
+    class Solution {
+        public int minEatingSpeed(int[] piles, int h) {
+            int max = 0;
+            for (int pile : piles) {
+                max = Math.max(max, pile);
+            }
+
+            int bad = 0;
+            int good = max + 1;
+
+            while (good - bad > 1) {
+                int k = bad + (good - bad) / 2;
+                if (canEat(k, piles, h)) {
+                    good = k;
+                } else {
+                    bad = k;
+                }
+            }
+
+            return good;
+        }
+
+        private boolean canEat(int k, int[] piles, int h) {
+            long resHours = 0;
+            for (int pile : piles) {
+                resHours += (pile + k - 1) / k;
+            }
+            return resHours <= h;
+        }
+    }
+}
