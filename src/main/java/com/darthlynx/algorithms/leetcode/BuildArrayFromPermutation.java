6b93890eca01cbda79c0fce8c7667a32ab61a4a8
Ihor Khlaponin <igor.boxmails@gmail.com>
2025-05-06 20:00:01 +0100

    leetcode 1920: build array from permutation

--- src/main/java/com/darthlynx/algorithms/leetcode/BuildArrayFromPermutation.java
+++ src/main/java/com/darthlynx/algorithms/leetcode/BuildArrayFromPermutation.java
@@ -1,10 +1,12 @@
 package com.darthlynx.algorithms.leetcode;
 
+// https://leetcode.com/problems/build-array-from-permutation/
 public class BuildArrayFromPermutation {
 
+    // Time complexity: O(n)
+    // Space complexity: O(n)
     public int[] buildArray(int[] nums) {
         int[] result = new int[nums.length];
-
         for (int i = 0; i < nums.length; i++) {
             result[i] = nums[nums[i]];
         }
