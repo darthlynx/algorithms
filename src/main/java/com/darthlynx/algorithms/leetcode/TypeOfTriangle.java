434671f03c6f9b7ee91b5e442b498c082128d1d7
Ihor Khlaponin <igor.boxmails@gmail.com>
2025-05-19 19:26:44 +0100

    leetcode 3024: type of triangle

---
+++ src/main/java/com/darthlynx/algorithms/leetcode/TypeOfTriangle.java
@@ -0,0 +1,30 @@
+package com.darthlynx.algorithms.leetcode;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/type-of-triangle/
+public class TypeOfTriangle {
+
+    // Time complexity: O(1)
+    // Space complexity: O(1)
+    class Solution {
+        public String triangleType(int[] nums) {
+            if (canNotForm(nums)) {
+                return "none";
+            }
+            if (nums[0] == nums[1] && nums[1] == nums[2]) {
+                return "equilateral";
+            }
+            if (nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) {
+                return "isosceles";
+            }
+            return "scalene";
+        }
+
+        private boolean canNotForm(int[] nums) {
+            return nums[0] + nums[1] <= nums[2]
+                    || nums[0] + nums[2] <= nums[1]
+                    || nums[1] + nums[2] <= nums[0];
+        }
+    }
+}
