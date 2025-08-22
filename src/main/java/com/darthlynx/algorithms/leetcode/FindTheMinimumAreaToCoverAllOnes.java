18fa1d4b0e7711468274af9990eedd0aa702b993
Ihor Khlaponin <igor.boxmails@gmail.com>
2025-08-22 23:44:46 +0100

    leetcode 3195: find the minimum area to cover all ones

---
+++ src/main/java/com/darthlynx/algorithms/leetcode/FindTheMinimumAreaToCoverAllOnes.java
@@ -0,0 +1,34 @@
+package com.darthlynx.algorithms.leetcode;
+
+// https://leetcode.com/problems/find-the-minimum-area-to-cover-all-ones-i/
+public class FindTheMinimumAreaToCoverAllOnes {
+
+    // Time complexity: O(n*m)
+    // Space complexity: O(1)
+    class Solution {
+        public int minimumArea(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            int minX = n;
+            int minY = m;
+            int maxX = 0;
+            int maxY = 0;
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == 1) {
+                        minX = Math.min(minX, j);
+                        maxX = Math.max(maxX, j);
+                        minY = Math.min(minY, i);
+                        maxY = Math.max(maxY, i);
+                    }
+                }
+            }
+
+            int x = maxX - minX + 1;
+            int y = maxY - minY + 1;
+
+            return x * y;
+        }
+    }
+}
