ddd1ca670b168a8a93614cb9ded0a00448ef9d2d
Ihor Khlaponin <igor.boxmails@gmail.com>
2025-09-23 22:38:21 +0100

    leetcode 165: compare version numbers

--- src/main/java/com/darthlynx/algorithms/leetcode/CompareVersionNumbers.java
+++ src/main/java/com/darthlynx/algorithms/leetcode/CompareVersionNumbers.java
@@ -14,20 +14,41 @@ public class CompareVersionNumbers {
             int p1 = 0, p2 = 0;
             while (result == 0) {
                 if (p1 < nums1.length && p2 < nums2.length) {
-                    result = Integer.compare(Integer.valueOf(nums1[p1]), Integer.valueOf(nums2[p2]));
+                    result = Integer.compare(Integer.parseInt(nums1[p1]), Integer.parseInt(nums2[p2]));
                     p1++;
                     p2++;
                 } else if (p1 < nums1.length && p2 >= nums2.length) {
-                    result = Integer.compare(Integer.valueOf(nums1[p1]), 0);
+                    result = Integer.compare(Integer.parseInt(nums1[p1]), 0);
                     p1++;
                 } else if (p2 < nums2.length && p1 >= nums1.length) {
-                    result = Integer.compare(0, Integer.valueOf(nums2[p2]));
+                    result = Integer.compare(0, Integer.parseInt(nums2[p2]));
                     p2++;
                 } else {
                     break;
                 }
             }
             return result;
+        }
+    }
+
+    // Time complexity: O(n + m)
+    // Space complexity: O(n + m)
+    class Solution2 {
+        public int compareVersion(String version1, String version2) {
+            String[] nums1 = version1.split("\\.");
+            String[] nums2 = version2.split("\\.");
+
+            int p1 = 0, p2 = 0;
+            while (p1 < nums1.length || p2 < nums2.length) {
+                int v1 = p1 < nums1.length ? Integer.parseInt(nums1[p1]) : 0;
+                int v2 = p2 < nums2.length ? Integer.parseInt(nums2[p2]) : 0;
+                if (v1 != v2) {
+                    return Integer.compare(v1, v2);
+                }
+                p1++;
+                p2++;
+            }
+            return 0;
         }
     }
 }
