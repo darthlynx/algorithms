652f27f0251e0faa1f08e313af1947052acfa3e8
Ihor Khlaponin <igor.boxmails@gmail.com>
2025-10-31 10:37:18 +0000

    leetcode 3289: the two sneaky numbers of digitville

--- src/main/java/com/darthlynx/algorithms/leetcode/TheTwoSneakyNumbersOfDigitville.java
+++ src/main/java/com/darthlynx/algorithms/leetcode/TheTwoSneakyNumbersOfDigitville.java
@@ -1,8 +1,8 @@
 package com.darthlynx.algorithms.leetcode;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 // https://leetcode.com/problems/the-two-sneaky-numbers-of-digitville/
 public class TheTwoSneakyNumbersOfDigitville {
@@ -13,17 +13,15 @@ public class TheTwoSneakyNumbersOfDigitville {
         public int[] getSneakyNumbers(int[] nums) {
             int[] result = new int[2];
             Arrays.fill(result, -1);
+            int index = 0;
 
-            Map<Integer, Integer> freq = new HashMap<>();
+            Set<Integer> visited = new HashSet<>();
 
             for (int num : nums) {
-                freq.put(num, freq.getOrDefault(num, 0) + 1);
-                if (freq.get(num) > 1) {
-                    if (result[0] >= 0) {
-                        result[1] = num;
+                if (!visited.add(num)) { // add() returns false if element already appeared in the set
+                    result[index++] = num;
+                    if (index > 1) {
                         break;
-                    } else {
-                        result[0] = num;
                     }
                 }
             }
