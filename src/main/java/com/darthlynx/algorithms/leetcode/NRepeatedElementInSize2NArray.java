499c370655f983c0b8ea6a3039aa58c22a339085
Ihor Khlaponin <igor.boxmails@gmail.com>
2026-01-02 16:58:48 +0000

    leetcode 961: N-Repeated Element in Size 2N array

--- src/main/java/com/darthlynx/algorithms/leetcode/NRepeatedElementInSize2NArray.java
+++ src/main/java/com/darthlynx/algorithms/leetcode/NRepeatedElementInSize2NArray.java
@@ -43,4 +43,44 @@ public class NRepeatedElementInSize2NArray {
             return -1;
         }
     }
+
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    class Solution3 {
+        public int repeatedNTimes(int[] nums) {
+            // check first 3 elements to find the repeated one
+            if (nums.length >= 3 && (nums[0] == nums[1] || nums[0] == nums[2])) {
+                return nums[0];
+            }
+
+            // apply Boyer-Moore Voting Algorithm for nums.length - 1 elements
+            int count = 0;
+            int elem = nums[0];
+            for (int i = 1; i < nums.length; i++) {
+                if (count == 0) {
+                    count++;
+                    elem = nums[i];
+                } else if (count > 0 && elem == nums[i]) {
+                    return elem;
+                } else {
+                    count--;
+                }
+            }
+
+            return elem;
+        }
+    }
+
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    class Solution4 {
+        public int repeatedNTimes(int[] nums) {
+            for (int i = 0; i < nums.length - 2; i++) {
+                if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2]) {
+                    return nums[i];
+                }
+            }
+            return nums[nums.length - 1];
+        }
+    }
 }
